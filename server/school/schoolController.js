7c558817a22286b946b15465f94101a4b3ce09e2
MartialDuv <33746332+MartialDuv@users.noreply.github.com>
2019-01-12 17:01:49 +0100

    ajout delete et get/:id pour school

--- server/school/schoolController.js
+++ server/school/schoolController.js
@@ -16,16 +16,42 @@ router.get('/', function (req, res) {
     });
 });
 
+router.get('/:id', function (req, res) {
+    School.getschool(req.params.id,function(err,row){
+        if(err) {
+            res.status(400).json(err);
+        }
+        else
+        {
+            res.json(row);
+        }
+    });
+});
+
 router.post('/', function (req, res) {
-    School.createschool(req.body,function(err,count){
+    School.createschool(req.body,function(err,success){
         if(err)
         {
             res.status(400).json(err);
         }
         else{
-            res.json(req.body);
+            res.send('School added succesfully !');
         }
     });
 });
+
+router.delete('/:id', function (req, res) {
+    School.deleteschool(req.params.id,function(err,success){
+        if(err)
+        {
+            res.status(400).json(err);
+        }
+        else{
+            res.send('School deleted succesfully !');
+        }
+    });
+});
+
+
 
 module.exports = router;
--- server/school/schoolModel.js
+++ server/school/schoolModel.js
@@ -6,11 +6,18 @@ var School = {
     {
         return db.query('select * from school', callback);
     },
+    getschool: function(Id, callback)
+    {
+        return db.query('select * from school where school_id=?', Id, callback);
+    },
     createschool: function (School, callback) {
         return db.query('insert into school(school_name, main_field, sub_field, academy, region, department, city,\
          type_diploma, diploma_name) values (?, ?, ?, ?, ?, ?, ?, ?, ?)',
          [School.school_name, School.main_field, School.sub_field, School.academy, School.region, School.department,
           School.city, School.type_diploma, School.diploma_name], callback);
+    },
+    deleteschool: function (Id, callback) {
+        return db.query('delete from school where school_id=?', Id, callback);
     }
 }
 
