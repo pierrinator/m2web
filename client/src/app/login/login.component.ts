a7f472b8ed1bb12b3d2244356e9e2b299667f922
MartialDuv <33746332+MartialDuv@users.noreply.github.com>
2019-01-12 18:34:50 +0100

    fix products

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent {
      this.http.get('https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics&rows=1000&sort=-rentree_lib&facet=gd_disciscipline_lib&facet=discipline_lib&facet=aca_etab_lib&facet=reg_ins_lib&facet=dep_ins_lib&facet=diplome_rgp&facet=uucr_ins_lib&facet=uucr_ins&facet=libelle_intitule_1&facet=etablissement_lib' )
   .subscribe((res: any[]) =>{
        console.log(res);
-       //this.products = res;
+       this.products = res["records"];
    });
   // this.http.get('http://localhost:3000/login' ).subscribe((res: any[]) =>{
   //     console.log(res);
