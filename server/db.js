efec059393d4cb200855d719beece5e76c866d82
MartialDuv <33746332+MartialDuv@users.noreply.github.com>
2019-01-12 16:19:11 +0100

    retrait duplication code.

--- server/db.js
+++ server/db.js
@@ -14,6 +14,17 @@ connection.connect(function(err) {
     }
    
     console.log('Connected to the MySQL server.');
+    connection.query(
+            'Create table IF NOT EXISTS school (school_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,\
+            school_name VARCHAR(80) NOT NULL, main_field VARCHAR(80) NOT NULL, sub_field VARCHAR(80) NOT NULL,\
+            academy VARCHAR(80) NOT NULL, region VARCHAR(80) NOT NULL, department VARCHAR(80) NOT NULL,\
+            city VARCHAR(80) NOT NULL, type_diploma VARCHAR(80) NOT NULL, diploma_name VARCHAR(80) NOT NULL)\
+            ENGINE=INNODB', function(err, results, fields) {
+    			if (err) {
+      				console.log(err.message);
+    			}
+  			});
+
   });
   
 
--- server/school/schoolModel.js
+++ server/school/schoolModel.js
@@ -4,25 +4,9 @@ var db = require('../db');
 var School = {
     getschools: function(callback)
     {
-        db.query(
-            'Create table IF NOT EXISTS school (school_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,\
-            school_name VARCHAR(80) NOT NULL, main_field VARCHAR(80) NOT NULL, sub_field VARCHAR(80) NOT NULL,\
-            academy VARCHAR(80) NOT NULL, region VARCHAR(80) NOT NULL, department VARCHAR(80) NOT NULL,\
-            city VARCHAR(80) NOT NULL, type_diploma VARCHAR(80) NOT NULL, diploma_name VARCHAR(80) NOT NULL)\
-            ENGINE=INNODB');
-        
         return db.query('select * from school', callback);
     },
     createschool: function (School, callback) {
-    	/* Create the table school if it doesn't exist */
-    	db.query(
-    		'Create table IF NOT EXISTS school (school_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,\
-            school_name VARCHAR(80) NOT NULL, main_field VARCHAR(80) NOT NULL, sub_field VARCHAR(80) NOT NULL,\
-            academy VARCHAR(80) NOT NULL, region VARCHAR(80) NOT NULL, department VARCHAR(80) NOT NULL,\
-            city VARCHAR(80) NOT NULL, type_diploma VARCHAR(80) NOT NULL, diploma_name VARCHAR(80) NOT NULL)\
-            ENGINE=INNODB');
-
-    	/* Insert a new school record and returns the callback. */
         return db.query('insert into school(school_name, main_field, sub_field, academy, region, department, city,\
          type_diploma, diploma_name) values (?, ?, ?, ?, ?, ?, ?, ?, ?)',
          [School.school_name, School.main_field, School.sub_field, School.academy, School.region, School.department,
