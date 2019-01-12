205ca8b8a70ecc449dd132931ef23c16523fc7f3
pierrinator <plorson3@gmail.com>
2019-01-12 14:47:28 +0100

    GET fonctionnel

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -15,11 +15,20 @@ export class LoginComponent {
   private password: string;
   private username: string;
   private tataa: string[];
+  private products  = [];
 
   constructor(private http: HttpClient) { }
 
   login() {
     console.log(`test`);
-    return this.http.get('http://localhost:3000/school');
+     this.http.get('https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics&rows=1000&sort=-rentree_lib&facet=gd_disciscipline_lib&facet=discipline_lib&facet=aca_etab_lib&facet=reg_ins_lib&facet=dep_ins_lib&facet=diplome_rgp&facet=uucr_ins_lib&facet=uucr_ins&facet=libelle_intitule_1&facet=etablissement_lib' )
+  .subscribe((res: any[]) =>{
+       console.log(res);
+       //this.products = res;
+   });
+  // this.http.get('http://localhost:3000/login' ).subscribe((res: any[]) =>{
+  //     console.log(res);
+  //     this.products = res;
+  // });
   }
 }
