1a5974c6d032672538b24367f648ddfc70455401
pierrinator <plorson3@gmail.com>
2019-01-13 12:53:16 +0100

    Nettoyage et affichage dans search

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -11,24 +10,12 @@ import { Observable } from 'rxjs';
 })
 
 export class LoginComponent {
-   url = 'http://localhost:3000';
   private password: string;
   private username: string;
-  private tataa: string[];
-  private products  = [];
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   login() {
-    console.log(`test`);
-     this.http.get('https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics&rows=1000&sort=-rentree_lib&facet=gd_disciscipline_lib&facet=discipline_lib&facet=aca_etab_lib&facet=reg_ins_lib&facet=dep_ins_lib&facet=diplome_rgp&facet=uucr_ins_lib&facet=uucr_ins&facet=libelle_intitule_1&facet=etablissement_lib' )
-  .subscribe((res: any[]) =>{
-       console.log(res);
-       this.products = res["records"];
-   });
-  // this.http.get('http://localhost:3000/login' ).subscribe((res: any[]) =>{
-  //     console.log(res);
-  //     this.products = res;
-  // });
-  }
+   }
+
 }
--- client/src/app/search/search.component.ts
+++ client/src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  private schools  = [];
+
+  constructor(private http: HttpClient) { }
+
+  getAllSchools() {
+    console.log(`test`);
+     this.http.get('https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics&rows=1000&sort=-rentree_lib&facet=gd_disciscipline_lib&facet=discipline_lib&facet=aca_etab_lib&facet=reg_ins_lib&facet=dep_ins_lib&facet=diplome_rgp&facet=uucr_ins_lib&facet=uucr_ins&facet=libelle_intitule_1&facet=etablissement_lib' )
+  .subscribe((res: any[]) =>{
+       console.log(res);
+       this.schools = res[`records`];
+  });
+  }
 
   ngOnInit() {
   }
