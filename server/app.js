32bfab3e61d3e9abe28ecf99a19b67152968a037
MartialDuv <33746332+MartialDuv@users.noreply.github.com>
2019-01-14 18:35:51 +0100

    fix CORS et implémentation delete

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ var schoolController = require('./school/schoolController');
 var userController = require('./user/userController');
 function myCors(req, res, nxt) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Content-Type, Accept, Accept-Language, Origin, User-Agent');
     if(req.method === 'OPTIONS') {
         res.sendStatus(204);
