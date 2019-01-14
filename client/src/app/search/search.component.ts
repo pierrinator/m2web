9e76da0568e1b01dc833ad293ca39ddfe6ccf86a
pierrinator <plorson3@gmail.com>
2019-01-14 19:43:42 +0100

    map

--- client/src/app/search/search.component.ts
+++ client/src/app/search/search.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+/// <reference types="googlemaps" />
+import { Component, Input, ViewChild, NgZone, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
 import {SelectionModel} from '@angular/cdk/collections';
 import {MatTableDataSource} from '@angular/material';
-
+import { MapsAPILoader, MouseEvent } from '@agm/core';
+import { LocationsService } from '../locations.service';
+import { MapsService } from '../maps.service';
+import { GoogleMapsAPIWrapper } from '@agm/core/services';
+declare const google: any;
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, AfterViewInit {
+
+  public searchControl: FormControl;
+
+  @ViewChild('search') public searchElementRef: ElementRef;
+
+  public lat: number;
+  public lng: number;
+  public zoom: number;
+
+  public openedWindow: number;
   
   private selectedName: string;
   private selectedMainfield: string;
@@ -50,11 +65,12 @@ export class SearchComponent implements OnInit {
 
    static initOptions = false;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,private locationService: LocationsService,
+    private mapApiLoader: MapsAPILoader,
+    private mapsService: MapsService,  private ngZone: NgZone) {
+       }
 
-  }
-
-  search() {
+  filter() {
     let urlGet = 'https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics&rows=100&sort=-rentree_lib&facet=gd_disciscipline_lib&facet=discipline_lib&facet=aca_etab_lib&facet=reg_ins_lib&facet=dep_ins_lib&facet=diplome_rgp&facet=uucr_ins_lib&facet=libelle_intitule_1&facet=etablissement_lib';
 
     if(this.selectedName != undefined) {
@@ -95,6 +111,33 @@ export class SearchComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    this.searchControl = new FormControl();
+    this.lat = this.mapsService.lat;
+    this.lng = this.mapsService.lng;
+    this.zoom = this.mapsService.zoom;
+
+    this.setCurrentPosition();
+    this.mapApiLoader.load();
+
+  
+    this.mapsService.newCoordinators.subscribe(
+      (coords: { lat: number, lng: number, zoom: number }) => {
+        if (coords) {
+          this.lat = coords.lat;
+          this.lng = coords.lng;
+          this.zoom = coords.zoom;
+          this.mapApiLoader.load();
+        }
+      }
+    );
+    // Open window after click on panel
+    this.mapsService.openWindow.subscribe(
+      index => {
+        this.openedWindow = +index;
+      }
+    );
+
     if(!SearchComponent.initOptions) {
       this.http.get((this.url) + `etablissement_lib&group_by=etablissement_lib`)
       .subscribe((res: any[]) =>{
@@ -150,6 +193,38 @@ export class SearchComponent implements OnInit {
       this.cities = SearchComponent.initCities;
       this.diploma_types = SearchComponent.initDiploma_types;
     }
+
+    
+  }
+
+  mapClicked($event: MouseEvent) {
+    console.log($event);
+  }
+
+  clickedMarker(label: string, index: number) {
+    console.log(`Clicked the marker: ${label || index}`);
+    this.openedWindow = index;
+  }
+
+  private setCurrentPosition() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = this.mapsService.lat = position.coords.latitude;
+        this.lng = this.mapsService.lng = position.coords.longitude;
+        this.zoom = 10;
+      });
+    }
+
+    // @Todo: resort the locations
+  }
+
+  isInfoWindowOpen(index: number) {
+    return this.openedWindow === index;
+  }
+
+  ngAfterViewInit(): void {
+    const test = google.maps.places.FindPlaceFromQueryRequest("Bordeaux");
+
   }
 
 }
