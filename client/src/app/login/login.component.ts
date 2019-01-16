932abb0183533065c2ad669c97e6dbdfb7f57a72
MartialDuv <33746332+MartialDuv@users.noreply.github.com>
2019-01-16 20:50:52 +0100

    fix erreur

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -25,6 +25,7 @@ export class LoginComponent implements OnInit {
     if (this.username == null || this.password == null){
       return alert('Vous devez rentrer un nom d\'utilisateur et un mot de passe');
     }
+    this.jwtservice.register(this.username, this.password);
    }
 
    ngOnInit(){
